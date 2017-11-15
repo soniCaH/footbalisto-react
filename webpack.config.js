703aebb343e02d2d95c2d1d16e9359db42a289ce
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2017-11-15 11:27:20 +0100

    Rework folder structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+   entry: './index.js',
+   output: {
+      path:'/',
+      filename: 'bundle.js',
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+     loaders: [
+       {
+         test: /\.jsx?$/,
+         exclude: /node_modules/,
+         loader: 'babel-loader',
+       query: {
+         presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+module.exports = config;
+
