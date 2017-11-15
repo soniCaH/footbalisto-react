e53986f31ee1328fd999daa8db4719ba6a0a9616
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2017-11-15 11:40:19 +0100

    Simple example for the frontend

--- App.js
+++ App.js
@@ -10,5 +10,29 @@ class App extends React.Component {
    }
 }
 
-export default App;
+class ItemLister extends React.Component {
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
+    
+    componentDidMount() {
+        fetch(`http://localhost:9000/seasons/1718/regions/bra/rankings/3C`) 
+            .then(result=> {
+                this.setState({items:result.json()});
+            });
+    }
+    
+    render() {        
+        return(
+            <div>
+                <div>Items:</div>
+                { this.state.items.map(item=> { return <div>{item.name}</div>}) }          
+            </div>  
+        );
+    }
+}
+
+//export default App;
+export default ItemLister;
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,16 @@ var config = {
    module: {
      loaders: [
        {
-         test: /\.jsx?$/,
+         test: /\.(js|jsx)?$/,
          exclude: /node_modules/,
          loader: 'babel-loader',
-       query: {
-         presets: ['es2015', 'react']
-            }
+         query: {
+         	presets: ['es2015', 'react']
          }
+       }, {
+         test: /\.scss$/,
+         loaders: ["style-loader", "css-loader", "sass-loader"]
+       }
       ]
    }
 }
